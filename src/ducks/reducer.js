440334f4daa8d925ddc5e53c3df04aea94dd3f14
Drew Miley <drewmiley92@gmail.com>
2019-05-04 14:17:57 +0100

    Implements simple reducers

--- src/ducks/reducer.js
+++ src/ducks/reducer.js
@@ -6,18 +6,45 @@ const setNumber = (state, number) => {
     return newState;
 }
 
+const loadQuiz = (state, quiz, code, leaderboard) => {
+    let newState = Object.assign({}, state);
+    newState.quiz = quiz;
+    newState.code = code;
+    newState.leaderboard = leaderboard;
+    return newState;
+}
+
+const generateQuiz = (state, quiz, code) => {
+    let newState = Object.assign({}, state);
+    newState.quiz = quiz;
+    newState.code = code;
+    return newState;
+}
+
+const setAnswer = (state, question, answer) => {
+    let newState = Object.assign({}, state);
+    // TODO: Implement
+    return newState;
+}
+
+const submitAnswers = (state, leaderboard) => {
+    let newState = Object.assign({}, state);
+    newState.leaderboard = leaderboard;
+    return newState;
+}
+
 export default function(state = {}, action) {
     switch (action.type) {
         case actiontypes.SET_NUMBER:
             return setNumber(state, action.number);
         case actiontypes.LOAD_QUIZ:
-            return state;
+            return loadQuiz(state);
         case actiontypes.GENERATE_QUIZ:
-            return state;
+            return generateQuiz(state);
         case actiontypes.SET_ANSWER:
-            return state;
+            return setAnswer(state);
         case actiontypes.SUBMIT_ANSWERS:
-            return state;
+            return submitAnswers(state);
         default:
             return state;
     }
