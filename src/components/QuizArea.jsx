aa5c22866aaded89baafd03bbacee6e092befd2a
Drew Miley <drewmiley92@gmail.com>
2019-05-08 22:56:16 +0100

    Hooks QuizArea

--- src/components/QuizArea.jsx
+++ src/components/QuizArea.jsx
@@ -2,28 +2,19 @@ import React, { Component } from 'react';
 
 import Question from './Question';
 
-export default class QuizArea extends Component {
-    constructor(props) {
-        super(props);
-        this.shouldComponentUpdate = (nextProps, nextState) => {
-            return true;
-        };
-    }
-
-    render() {
-        return (
-            <div id="quiz-area">
-                <div id="quiz-code">{this.props.code}</div>
-                {this.props.quiz.map((d, i) =>
-                    <Question
-                        key={i}
-                        setAnswer={this.props.setAnswer}
-                        question={d.question}
-                        selectedAnswer={this.props.answers.find(qa => qa.question == d.question)}
-                        answers={[d.answer].concat(d.incorrectAnswers).sort(() => 0.5 - Math.random())}
-                    />
-                )}
-            </div>
-        );
-    }
-};
+export default props => {
+    return (
+        <div id="quiz-area">
+            <div id="quiz-code">{props.code}</div>
+            {props.quiz.map((d, i) =>
+                <Question
+                    key={i}
+                    setAnswer={props.setAnswer}
+                    question={d.question}
+                    selectedAnswer={props.answers.find(qa => qa.question == d.question)}
+                    answers={[d.answer].concat(d.incorrectAnswers).sort(() => 0.5 - Math.random())}
+                />
+            )}
+        </div>
+    );
+}
