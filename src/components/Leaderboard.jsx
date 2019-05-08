ff80961b576bb186a2ac7d0f14d6bad7d39808c6
Drew Miley <drewmiley92@gmail.com>
2019-05-08 22:34:44 +0100

    Hooks leaderboard

--- src/components/Leaderboard.jsx
+++ src/components/Leaderboard.jsx
@@ -1,20 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class Leaderboard extends Component {
-    constructor(props) {
-        super(props);
-        this.shouldComponentUpdate = (nextProps, nextState) => {
-            return true;
-        };
-    }
+const Leaderboard = props => {
+    return (
+        <div id="leaderboard">
+            {props.leaderboard.sort((a, b) => b.score - a.score).map((d, i) =>
+                <div key={i}>{d.user} - {d.score}</div>
+            )}
+        </div>
+    );
+}
 
-    render() {
-        return (
-            <div id="leaderboard">
-                {this.props.leaderboard.sort((a, b) => b.score - a.score).map((d, i) =>
-                    <div key={i}>{d.user} - {d.score}</div>
-                )}
-            </div>
-        );
-    }
-};
+export default Leaderboard;
