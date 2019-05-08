ff904b5d97e2319bb7365c84eba33461c92f56c4
Drew Miley <drewmiley92@gmail.com>
2019-05-08 22:53:12 +0100

    Hooks InitQuiz

--- src/components/InitQuiz.jsx
+++ src/components/InitQuiz.jsx
@@ -1,26 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import GenerateQuiz from './GenerateQuiz';
 import LoadQuiz from './LoadQuiz';
 
-export default class InitQuiz extends Component {
-    constructor(props) {
-        super(props);
-        this.shouldComponentUpdate = (nextProps, nextState) => {
-            return true;
-        };
-    }
-
-    render() {
-        return (
-            <div id="quiz-init">
-                <LoadQuiz
-                    loadQuiz={this.props.loadQuiz}
-                />
-                <GenerateQuiz
-                    generateQuiz={this.props.generateQuiz}
-                />
-            </div>
-        );
-    }
-};
+export default props => {
+    return (
+        <div id="quiz-init">
+            <LoadQuiz
+                loadQuiz={props.loadQuiz}
+            />
+            <GenerateQuiz
+                generateQuiz={props.generateQuiz}
+            />
+        </div>
+    );
+}
