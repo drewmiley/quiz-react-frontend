e0338a6657667f7226f92c708d2bda52853d9ec9
Drew Miley <drewmiley92@gmail.com>
2019-05-09 00:25:31 +0100

    attempt to deploy to heroku

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 8081;
+const app = express();
+
+const dist = __dirname + '/dist';
+
+// the __dirname is the current directory from where the script is running
+app.use(express.static(dist));
+
+// send the user to index html page inspite of the url
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(dist, 'index.html'));
+});
+
+app.listen(port);
