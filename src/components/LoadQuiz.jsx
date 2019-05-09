912703d51d768ef5d6ee482a58c57b49608719f0
Drew Miley <drewmiley92@gmail.com>
2019-05-10 00:06:25 +0100

    Implements load code sleect

--- src/components/LoadQuiz.jsx
+++ src/components/LoadQuiz.jsx
@@ -1,34 +1,13 @@
-import React, { useState } from 'react';
-
-// const friendList = [
-//   { id: 1, name: 'Phoebe' },
-//   { id: 2, name: 'Rachel' },
-//   { id: 3, name: 'Ross' },
-// ];
-//
-// function ChatRecipientPicker() {
-//   const [recipientID, setRecipientID] = useState(1);
-//   const isRecipientOnline = useFriendStatus(recipientID);
-//
-//   return (
-//     <>
-//       <Circle color={isRecipientOnline ? 'green' : 'red'} />
-//       <select
-//         value={recipientID}
-//         onChange={e => setRecipientID(Number(e.target.value))}
-//       >
-//         {friendList.map(friend => (
-//           <option key={friend.id} value={friend.id}>
-//             {friend.name}
-//           </option>
-//         ))}
-//       </select>
-//     </>
-//   );
-// }
+import React, { useEffect, useState } from 'react';
 
 export default props => {
     const [value, setValue] = useState('');
+
+    useEffect(() => {
+        if (!value) {
+            setValue(props.validQuizCodes[0]);
+        }
+    }, [props.validQuizCodes]);
 
     const onChange = e => setValue(e.target.value);
 
@@ -36,13 +15,13 @@ export default props => {
 
     return (
         <div id="quiz-loader">
-            {props.validQuizCodes.map(code =>
-                <>
-                    <p>{code}</p>
-                    <p>CHANGE THIS INPUT TO A SELECT</p>
-                </>
-            )}
-            <input id="code" type="text" value={value} onChange={onChange} />
+            <select id="code" value={value} onChange={onChange} >
+                {props.validQuizCodes.map(code =>
+                    <option key={code} value={code}>
+                        {code}
+                    </option>
+                )}
+            </select>
             <button id="load" onClick={onClick}>Load</button>
         </div>
     );
