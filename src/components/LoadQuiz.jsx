160d82e678855743dc11c2ad3720f292864c25a3
Drew Miley <drewmiley92@gmail.com>
2019-05-11 17:03:07 +0100

    Autocomplete works

--- src/components/LoadQuiz.jsx
+++ src/components/LoadQuiz.jsx
@@ -3,42 +3,19 @@ import AutoComplete from 'react-autocomplete';
 
 export default props => {
     const [value, setValue] = useState('');
-    const [value1, setValue1] = useState('');
-
-    useEffect(() => {
-        if (!value) {
-            setValue(props.validQuizCodes[0]);
-        }
-    }, [props.validQuizCodes]);
-
-    const onChange = e => setValue(e.target.value);
 
     const onClick = () => props.loadQuiz(value);
 
     return (
         <div id="quiz-loader">
-            <select id="code" value={value} onChange={onChange} >
-                {props.validQuizCodes.map(code =>
-                    <option key={code} value={code}>
-                        {code}
-                    </option>
-                )}
-            </select>
             <AutoComplete
-              getItemValue={(item) => item.label}
-              items={[
-                { label: 'apple', key:0 },
-                { label: 'banana', key:1 },
-                { label: 'pear', key:2 }
-              ]}
-              renderItem={(item, isHighlighted) =>
-                <div style={{ background: isHighlighted ? 'lightgray' : 'white' }}>
-                  {item.label}
-                </div>
-              }
-              value={value1}
-              onChange={(e) => setValue1(e.target.value)}
-              onSelect={(val) => setValue1(val)}
+                getItemValue={item => item}
+                items={props.validQuizCodes}
+                renderItem={(item, isHighlighted) =><div style={{ background: isHighlighted ? 'lightgray' : 'white' }} key={item}>{item}</div>}
+                shouldItemRender={(item, value) => item.includes(value)}
+                value={value}
+                onChange={e => setValue(e.target.value)}
+                onSelect={val => setValue(val)}
             />
             <button id="load" onClick={onClick}>Load</button>
         </div>
