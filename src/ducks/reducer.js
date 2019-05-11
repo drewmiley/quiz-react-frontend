0cbec307087882ac5f0eaaada276b743b781cd04
Drew Miley <drewmiley92@gmail.com>
2019-05-11 12:18:37 +0100

    Uses payload in reducer

--- src/ducks/reducer.js
+++ src/ducks/reducer.js
@@ -1,45 +1,45 @@
 import * as actiontypes from './actiontypes';
 
-const loadQuiz = state => (quiz, code, leaderboard) => {
-    state.quiz = quiz;
-    state.code = code;
-    state.leaderboard = leaderboard;
+const loadQuiz = state => payload => {
+    state.quiz = payload.quiz;
+    state.code = payload.code;
+    state.leaderboard = payload.leaderboard;
     return state;
 }
 
-const generateQuiz = state => (quiz, code) => {
-    state.quiz = quiz;
-    state.code = code;
+const generateQuiz = state => payload => {
+    state.quiz = payload.quiz;
+    state.code = payload.code;
     return state;
 }
 
-const setAnswer = state => (question, answer) => {
-    const answers = state.answers.find(d => d.question == question) ?
+const setAnswer = state => payload => {
+    const answers = state.answers.find(d => d.question == payload.question) ?
         state.answers.map(d => {
-            return d.question == question ? { question, answer } : d;
+            return d.question == payload.question ? payload : d;
         }) :
-        state.answers.concat([{ question, answer }]);
+        state.answers.concat([payload]);
     state.answers = answers;
     return state;
 }
 
-const submitAnswers = state => leaderboard => {
-    state.leaderboard = leaderboard;
+const submitAnswers = state => payload => {
+    state.leaderboard = payload.leaderboard;
     return state;
 }
 
-const getLeaderboards = state => leaderboards => {
-    state.leaderboards = leaderboards;
+const getLeaderboards = state => payload => {
+    state.leaderboards = payload.leaderboards;
     return state;
 }
 
-const getValidQuizCodes = state => validQuizCodes => {
-    state.validQuizCodes = validQuizCodes;
+const getValidQuizCodes = state => payload => {
+    state.validQuizCodes = payload.validQuizCodes;
     return state;
 }
 
-const getValidQuizOptions = state => validQuizOptions => {
-    state.validQuizOptions = validQuizOptions;
+const getValidQuizOptions = state => payload => {
+    state.validQuizOptions = payload.validQuizOptions;
     return state;
 }
 
@@ -47,19 +47,19 @@ export default function(state = {}, action) {
     const newState = () => Object.assign({}, state);
     switch (action.type) {
         case actiontypes.LOAD_QUIZ:
-            return loadQuiz(newState())(action.payload.quiz, action.payload.code, action.payload.leaderboard);
+            return loadQuiz(newState())(action.payload);
         case actiontypes.GENERATE_QUIZ:
-            return generateQuiz(newState())(action.payload.quiz, action.payload.code);
+            return generateQuiz(newState())(action.payload);
         case actiontypes.SET_ANSWER:
-            return setAnswer(newState())(action.payload.question, action.payload.answer);
+            return setAnswer(newState())(action.payload);
         case actiontypes.SUBMIT_ANSWERS:
-            return submitAnswers(newState())(action.payload.leaderboard);
+            return submitAnswers(newState())(action.payload);
         case actiontypes.GET_LEADERBOARDS:
-            return getLeaderboards(newState())(action.payload.leaderboards);
+            return getLeaderboards(newState())(action.payload);
         case actiontypes.GET_VALIDQUIZCODES:
-            return getValidQuizCodes(newState())(action.payload.validQuizCodes);
+            return getValidQuizCodes(newState())(action.payload);
         case actiontypes.GET_VALIDQUIZOPTIONS:
-            return getValidQuizOptions(newState())(action.payload.validQuizOptions);
+            return getValidQuizOptions(newState())(action.payload);
         default:
             return newState();
     }
