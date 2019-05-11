8e9f4dc2765dc8f2a9072af49b6f6217548a4c69
Drew Miley <drewmiley92@gmail.com>
2019-05-11 17:10:10 +0100

    Moves out position into set field

--- src/components/Leaderboard.jsx
+++ src/components/Leaderboard.jsx
@@ -4,9 +4,15 @@ export default props => {
     return (
         <div id="leaderboard">
             <h2>{props.code} LEADERBOARD</h2>
-            {props.leaderboard.sort((a, b) => b.score - a.score).map((d, i) =>
-                <div key={i}>{i + 1} - {d.user} - {d.score}</div>
-            )}
+            {props.leaderboard.sort((a, b) => b.score - a.score)
+                .map((d, i) => {
+                    const position = i + 1;
+                    return { position, user: d.user, score: d.score };
+                })
+                .map((d, i) =>
+                    <div key={i}>{d.position} - {d.user} - {d.score}</div>
+                )
+            }
         </div>
     );
 }
