ad03d51c9a4d590494c0e0920eed95596b66331e
Drew Miley <drewmiley92@gmail.com>
2019-05-12 22:40:57 +0100

    Adds naive implementation of react routes

--- src/AppContainer.jsx
+++ src/AppContainer.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import { mapDispatchToProps } from './ducks/actions';
 
@@ -9,11 +10,7 @@ import Leaderboards from './components/Leaderboards';
 import QuizArea from './components/QuizArea';
 import SubmitAnswers from './components/SubmitAnswers';
 
-export default class App extends Component {
-    componentDidMount() {
-        this.props.getValidQuizCodes();
-        this.props.getValidQuizOptions();
-    }
+class Home extends Component {
     render() {
         return <>
             <InitQuiz
@@ -42,5 +39,51 @@ export default class App extends Component {
         </>
     }
 };
+
+const About = () => {
+  return <h2>About</h2>;
+}
+
+const Users = () => {
+  return <h2>Users</h2>;
+}
+
+class App extends Component {
+    componentDidMount() {
+        this.props.getValidQuizCodes();
+        this.props.getValidQuizOptions();
+    }
+    render() {
+        return (
+            <Router>
+                <>
+                    <nav>
+                        <ul>
+                            <li>
+                                <Link to="/">Home</Link>
+                            </li>
+                            <li>
+                                <Link to="/about/">About</Link>
+                            </li>
+                            <li>
+                                <Link to="/users/">Users</Link>
+                            </li>
+                        </ul>
+                    </nav>
+
+                    <Route
+                        path="/"
+                        exact
+                        render={(routeProps) => (
+                            <Home {...this.props} />
+                        )}
+                    />
+                    <Route path="/about/" component={About} />
+                    <Route path="/users/" component={Users} />
+                </>
+            </Router>
+        )
+    }
+}
 
 export const AppContainer = connect(state => state, mapDispatchToProps)(App);
