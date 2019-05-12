787eb849efc76cdc2d937fb50cd367570facdb82
Drew Miley <drewmiley92@gmail.com>
2019-05-12 22:00:19 +0100

    Position in leaderboard

--- src/components/Leaderboard.jsx
+++ src/components/Leaderboard.jsx
@@ -5,12 +5,14 @@ export default props => {
         <div id="leaderboard">
             <h2>{props.code} LEADERBOARD</h2>
             {props.leaderboard.sort((a, b) => b.score - a.score)
-                .map((d, i) => {
-                    const position = i + 1;
-                    return { position, user: d.user, score: d.score };
-                })
-                .map((d, i) =>
-                    <div key={i}>{d.position} - {d.user} - {d.score}</div>
+                .reduce((acc, d) => {
+                    const position = acc.length && d.score == acc[acc.length - 1].score ?
+                        acc[acc.length - 1].position :
+                        acc.length + 1;
+                    return acc.concat([{ position, user: d.user, score: d.score }]);
+                }, [])
+                .map(d =>
+                    <div key={d.user}>{d.position} - {d.user} - {d.score}</div>
                 )
             }
         </div>
