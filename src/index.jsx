7238bf993183b42d54c828601dc05d341786d0bf
Drew Miley <drewmiley92@gmail.com>
2019-05-12 23:26:12 +0100

    Dummy code to alter

--- src/index.jsx
+++ src/index.jsx
@@ -6,6 +6,20 @@ import {AppContainer} from './AppContainer';
 
 import store from './ducks/store';
 
+// import { PersistGate } from 'redux-persist/integration/react'
+//
+// // ... normal setup, create store and persistor, import components etc.
+//
+// const App = () => {
+//   return (
+//     <Provider store={store}>
+//       <PersistGate loading={null} persistor={persistor}>
+//         <RootComponent />
+//       </PersistGate>
+//     </Provider>
+//   );
+// };
+
 ReactDOM.render(
     <Provider store={store}>
         <AppContainer />
