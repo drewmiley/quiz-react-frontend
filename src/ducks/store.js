3f1bbfa303aaa61c612e4dcb5105d702ba4e8604
Drew Miley <drew.miley@opencastsoftware.com>
2019-06-12 15:34:25 +0100

    Install redux-anon-reducer from npm

--- src/ducks/store.js
+++ src/ducks/store.js
@@ -1,4 +1,4 @@
-import anonReducer from '../../redux-anon-reducer';
+import anonReducer from 'redux-anon-reducer';
 import { createStore, applyMiddleware } from 'redux';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
